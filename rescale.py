5e1bd1ffee079b2369ac9b2b05e40af26966afc6
BinhPhuc <phamphucbinh_hsgs2021@hus.edu.vn>
2023-09-03 10:55:27 +0700

    basics

--- rescale.py
+++ rescale.py
@@ -3,6 +3,7 @@ import cv2
 img = cv2.imread('Photos/cat.jpg')
 
 def rescale(frame, scale=0.7):
+    # Images, Videos and Live Videos
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
     dimensions = (width, height)
@@ -10,4 +11,20 @@ def rescale(frame, scale=0.7):
     # Short hand
     # return cv2.resize(frame, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
 
-cv2.waitKey(0)
+def changeRes(width, height):
+    # Live videos
+    capture.set(3, width)
+    capture.set(4, height)
+
+capture = cv2.VideoCapture('Videos/1.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    frame_resized = rescale(frame, scale=0.7)
+    cv2.imshow("video", frame)
+    cv2.imshow("video_resized", frame_resized)
+    if cv2.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
